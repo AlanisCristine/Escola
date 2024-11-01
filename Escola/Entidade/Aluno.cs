c713c80753811157cd19642db623526c9b2d5f73
Alanis Cristine <163910391+AlanisCristine@users.noreply.github.com>
2024-11-01 16:16:10 -0300

    InicializadorBD

--- Escola/Entidade/Aluno.cs
+++ Escola/Entidade/Aluno.cs
@@ -10,6 +10,7 @@ namespace Escola.Entidade
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public int Idade { get; set; }
         public double Peso { get; set; }
         public double Altura { get; set; }
     }
---
+++ Escola/Repository/InicializadorBD.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Repository;
+
+public class InicializadorBD
+{
+    private const string ConnectionString = "Data Source=Supermercado.db";
+
+    public static void Inicializar()
+    {
+        using (var connection = new SQLiteConnection(ConnectionString))
+        {
+            connection.Open();
+            string commandoSQL = @"   
+                 CREATE TABLE IF NOT EXISTS Alunos(
+                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 Nome TEXT NOT NULL,
+                 Idade INTEGER NOT NULL,
+                 Peso REAL NOT NULL,
+                 Altura REAL NOT NULL
+                );";
+
+            connection.Execute(commandoSQL);
+        }
+    }
+}
